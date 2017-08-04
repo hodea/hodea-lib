1d11159815f5e9b65d920757beab7b5936d3a5bf
Franz Hollerer <f.hollerer@gmx.net>
2017-08-04 23:08:53 +0200

    make support added

---
+++ make/posix_hosted_build.mk
@@ -0,0 +1,22 @@
+# Copyright (c) 2017, Franz Hollerer.
+# This code is licensed under the MIT License (MIT).
+# See LICENSE file for full details.
+
+# This makefile is executed within the build directory to build the
+# given target for a "posix_hosted" system.
+
+C_OBJ_FILES := $(addsuffix .o, $(C_SOURCE_FILES))
+CXX_OBJ_FILES := $(addsuffix .o, $(CXX_SOURCE_FILES))
+
+-include $(C_OBJ_FILES:.o=.d)
+-include $(CXX_OBJ_FILES:.o=.d)
+
+$(TARGET): $(C_OBJ_FILES) $(CXX_OBJ_FILES)
+	$(CXX) $(LDFLAGS) $^ -o $@
+
+$(C_OBJ_FILES):
+	$(CC) $(CFLAGS) -c $(SOURCE_ROOT_DIR)/$(basename $@) -o $@
+
+$(CXX_OBJ_FILES):
+	$(CXX) $(CXXFLAGS) -c $(SOURCE_ROOT_DIR)/$(basename $@) -o $@
+
