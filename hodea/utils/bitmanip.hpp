7c0aa9bb579b09a972c2cb4580bbb007d3d0df76
Franz Hollerer <f.hollerer@gmx.net>
2017-08-06 23:26:42 +0200

    build support updated

---
+++ hodea/utils/bitmanip.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2017, Franz Hollerer. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// See LICENSE file for full details.
+
+/**
+ * Functions for bit manipulation.
+ *
+ * This file provides several template functions for bit manipulation,
+ * e.g. to set, clear or test a bit (or multiple bits) within a variable.
+ *
+ * The templates ensure that the operations are performed on unsigned
+ * types to avoid surprising effects cause by integer propagation.
+ *
+ * @author f.hollerer@gmx.net
+ */
+ #if !defined _HODEA_BITMANIP_HPP_
+ #define _HODEA_BITMANIP_HPP_
+
+ static inline int foo() { return 0x0815; }
+
+ #endif /*!_HODEA_BITMANIP_HPP_ */
+
+
+
